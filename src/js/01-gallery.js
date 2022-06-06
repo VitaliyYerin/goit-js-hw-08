11e43b6977404d0649a6f580d22000d44424602a
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-06-06 11:50:45 +0300

    change js file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import {galleryItems} from './gallery-items.js';
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Дополнительный импорт стилей
 // Change code below this line
-
-console.log(galleryItems);
+// const galleryList = document.querySelector('.gallery');
+// const galleryElements = createGallery(galleryItems);
+// galleryList.insertAdjacentHTML('afterbegin', galleryElements);
+// galleryList.addEventListener('click', onGallery);
+// const gallery = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: 'alt', docClose: true});
+//
+// function createGallery(galleryItems) {
+//   return galleryItems.map(({preview, original, description}) => {
+//     return `<a class="gallery__item" href="${original}" onclick = "event.preventDefault()">
+//                 <img
+//                 class="gallery__image"
+//                 src="${preview}"
+//                 alt="${description}"
+//                 />
+//                 </a>`;
+//   })
+//     .join('');
+// }
+//
+// function onGallery(e) {
+//   e.stopPropagation();
+//
+//   if (gallery) {
+//     gallery.show(e.target.closest('a'));
+//   }
+// }
